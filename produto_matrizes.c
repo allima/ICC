e2433233f43885efc7ee1cb0cd224ce0de1e6909
Alexandre de Lima <allima1991@gmail.com>
2024-10-19 19:54:57 -0300

    ajuste remoção de comentarios

--- produto_matrizes.c
+++ produto_matrizes.c
@@ -27,27 +27,6 @@ void main()
          scanf("%lf", &b[i][j]);
       }
    }
-   /*
-
-
-   for (int i = 0; i < 3; i++)
-   {
-      for (int j = 0; j < 3; j++)
-      {
-         a[i][j]=3;
-      }
-
-   }
-   for (int i = 0; i < 3; i++)
-   {
-      for (int j = 0; j < 3; j++)
-      {
-         b[i][j]=3;
-      }
-
-   }
-
-*/
 
    for (int i = 0; i < 3; i++)
    {
